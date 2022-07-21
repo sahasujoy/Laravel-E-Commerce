d1eff1cbe740d5438e7440ef089127f64bea6ea7
sahasujoy <sahasujoy.cse@gmail.com>
2022-07-21 09:43:49 +0600

    Login Done with Middleware

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,14 +5,15 @@
     <h3>Login</h3>
     <div class="row">
         <div class="col-sm-4 col-sm-offset">
-            <form action="">
+            <form action="{{ url('/login') }}" method="POST">
+                @csrf
                 <div class="form-group">
                     <label for="inputEmail1">Email Address</label>
-                    <input type="email" class="form-control" id="inputEmail1" placeholder="Enter Your Email">
+                    <input type="email" name="email" class="form-control" id="inputEmail1" placeholder="Enter Your Email">
                 </div>
                 <div class="form-group">
                     <label for="inputPassword1">Password</label>
-                    <input type="password" class="form-control" id="inputPassword1" placeholder="Enter Your Password">
+                    <input type="password" name="password" class="form-control" id="inputPassword1" placeholder="Enter Your Password">
                 </div>
                 <button type="submit" class="btn btn-default">Submit</button>
             </form>
